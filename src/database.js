3421e623231f10aa7eba598f19792ede7444edc8
ZurdoAsd <gaspar.ajs@gmail.com>
2023-08-29 09:59:37 -0300

    mod connect

--- src/database.js
+++ src/database.js
@@ -1,12 +1,27 @@
 const mongoose =require ("mongoose")
+require('dotenv').config();
+const {
+    MONGODB_USER,
+    MONGODB_PASS,
+    MONGODB_CLUSTER,
+    MONGODB_DATABASE,
+    MONGODB_PORT,
+  } = process.env;
 
-const contactadb= process.env.MONGO_DB
+
+//const contactadb= process.env.MONGO_DB
 //modulo conect contecta con la instancia
-mongoose.connect(contactadb)
+//mongoose.connect(contactadb)
+mongoose.connect(
+`mongodb://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_CLUSTER}:${MONGODB_PORT}/${MONGODB_DATABASE}?authSource=admin`,
+)
 
 const connect = mongoose.connection
 
-connect.once("open", ()=>{ console.log("db is conected")})
+connect
+.once("open", ()=>{ console.log("db is conected")})
+.catch((error) => console.error(error));
+
 
 
 module.exports = mongoose
